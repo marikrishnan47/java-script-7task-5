398295cd26e446e15f02c0f8a2e84dfd7afb40d2
marikrishnan47 <krishmarish028@gmail.com>
2023-02-06 16:29:40 +0530

    flie

---
+++ java-5.js
@@ -0,0 +1,15 @@
+// Using reduce method............
+
+const numbers = [1, 2, 3, 4, 5];
+let num=numbers.reduce(function(fun,val){
+	  
+	 
+	  return fun+val+5
+	  
+	   
+},0);
+
+
+console.log(num)
+  
+  
